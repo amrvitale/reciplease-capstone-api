7b8bce1fe2a8ec35f3698620c78712889a0b06b1
Angela Bumgarner <amrvitale@gmail.com>
2020-07-02 01:37:59 -0700

    got table to be created

--- seeds/seed.recipes.sql
+++ seeds/seed.recipes.sql
@@ -1,4 +1,4 @@
 INSERT INTO recipes ("recipename","experience", "preptime","cooktime", "ingredients", "directions")
 VALUES
-('Pasta alla norma', 'This was not at all difficult, and so fun to make!','30 minutes','45 minutes', 'eggplant, canned tomatoes, yellow raisins, tomato sauce, pine nuts, capers, garlic, onion, basil, parmesan,rotini', 'Boil pasta according to directions' );
+('Pasta alla norma', 'This was not at all difficult and so fun to make','thirty minutes','forty minutes', 'eggplant canned tomatoes yellow raisins tomato sauce pine nuts capers garlic onion basil parmesan rotini', 'Boil pasta according to directions' );
 
